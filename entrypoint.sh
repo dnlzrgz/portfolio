3e967c862aa58bbe29315c9128af0260a05df0fd
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-05-17 17:10:46 +0200

    feat: add favicon and basic og tags

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,6 @@ set -o nounset
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 
 gunicorn portfolio.wsgi:application --bind 0.0.0.0:$PORT
