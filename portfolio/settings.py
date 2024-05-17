0008ec2f986a6c1f1109d096caca6a075ca6edec
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-05-17 18:42:24 +0200

    fix(docker): solve issue with env variables

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -258,7 +258,7 @@ WAGTAILSEARCH_BACKENDS = {
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-WAGTAILADMIN_BASE_URL = "http://example.com"
+WAGTAILADMIN_BASE_URL = env.str("WAGTAILADMIN_BASE_URL", "http://example.com")
 
 # Allowed file extensions for documents in the document library.
 # This can be omitted to allow all files, but note that this may present a security risk
