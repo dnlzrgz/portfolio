a31dc97d3d6b771bc7a757cf9e3fbe5e85f310c9
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-05-24 10:22:48 +0200

    feat(base): add static pages

--- base/models.py
+++ base/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 from wagtail import blocks
+from wagtail.models import Page, StreamField
 from wagtail.admin.panels import FieldPanel
-from wagtail.models import StreamField
+from wagtail.fields import RichTextField
 from wagtail.contrib.settings.models import (
     BaseGenericSetting,
     register_setting,
 )
+
+
+class StaticPage(Page):
+    body = RichTextField(blank=True, editor="full")
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
+
+    parent_page_types = ["home.HomePage"]
+    subpage_types = []
 
 
 @register_setting
