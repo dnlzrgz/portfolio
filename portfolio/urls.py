e213d816daeac1a307d80da1496b3680331be6cb
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-05-24 12:21:09 +0200

    feat: add sitemap

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from search import views as search_views
 from blog.feeds import RssFeed, AtomFeed
 
@@ -36,5 +37,6 @@ if settings.DEBUG:
 
 
 urlpatterns = urlpatterns + [
+    path("sitemap.xml", sitemap),
     path("", include(wagtail_urls)),
 ]
