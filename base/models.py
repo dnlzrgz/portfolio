4ba48e9e68618a86900655367808973ccd7551b1
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-10-18 17:45:45 +0200

    feat(base): add base site settings

--- base/models.py
+++ base/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from wagtail import blocks
-from wagtail.models import Page, StreamField
+from wagtail.models import ClusterableModel, Page, StreamField
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
 from wagtail.contrib.settings.models import (
     BaseGenericSetting,
+    BaseSiteSetting,
     register_setting,
 )
 
@@ -20,36 +21,51 @@ class StaticPage(Page):
     subpage_types = []
 
 
-@register_setting
+@register_setting(icon="mail")
 class ContactEmailSettings(BaseGenericSetting):
     email = models.EmailField(blank=True)
 
     panels = [FieldPanel("email")]
 
 
-@register_setting
-class FooterSettings(BaseGenericSetting):
-    content = models.CharField(blank=True, max_length=255)
-    location = models.CharField(blank=True, max_length=255)
-    copy = models.CharField(blank=True, max_length=255)
+@register_setting(icon="cog")
+class FooterTextSettings(ClusterableModel, BaseGenericSetting):
+    body = RichTextField(blank=True)
+
+    panels = [
+        FieldPanel("body"),
+    ]
+
+
+@register_setting(icon="cog")
+class SocialMediaSettings(ClusterableModel, BaseGenericSetting):
     links = StreamField(
         [
             (
-                "url",
+                "link",
                 blocks.StructBlock(
                     [
-                        ("title", blocks.CharBlock()),
+                        ("name", blocks.CharBlock()),
                         ("url", blocks.URLBlock()),
-                    ]
+                    ],
                 ),
             )
         ],
         blank=True,
     )
 
+    panels = [FieldPanel("links")]
+
+
+@register_setting(icon="site")
+class SiteSettings(BaseSiteSetting):
+    title_suffix = models.CharField(
+        verbose_name="Title suffix",
+        max_length=255,
+        help_text="The suffix for the title meta tag e.g. ' | dnlzrgz'",
+        default="dnlzrgz",
+    )
+
     panels = [
-        FieldPanel("content"),
-        FieldPanel("location"),
-        FieldPanel("copy"),
-        FieldPanel("links"),
+        FieldPanel("title_suffix"),
     ]
