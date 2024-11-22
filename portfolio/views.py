7d44cb7884fe9ece1015354f131f52f8b3aeebbc
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-11-22 10:01:18 +0100

    feat: add 404 and 500 buildable views

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,26 @@ from bakery.views import BuildableTemplateView
 from wagtail.contrib.sitemaps.views import sitemap
 
 
+class Custom404View(BuildableTemplateView):
+    build_path = "404.html"
+    template_name = "error.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["status_code"] = "404"
+        return context
+
+
+class Custom500View(BuildableTemplateView):
+    build_path = "500.html"
+    template_name = "error.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["status_code"] = "500"
+        return context
+
+
 class RobotsView(BuildableTemplateView):
     build_path = "robots.txt"
     template_name = "robots.txt"
