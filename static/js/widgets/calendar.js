0a6f2877776ef4b1b5b969587fcfae17f89d7134
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2025-01-26 18:43:04 +0100

    fix(base): change calendar's next and prev button classes

--- static/js/widgets/calendar.js
+++ static/js/widgets/calendar.js
@@ -16,20 +16,19 @@
       year: "numeric",
     });
 
-    daysList.innerHTML = "";
-
+    const fragment = document.createDocumentFragment();
     const firstDay = new Date(year, month, 1).getDay(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
     const daysInMonth = new Date(year, month + 1, 0).getDate();
 
     for (let i = 0; i < firstDay; i++) {
       const emptyCell = document.createElement("li");
-      emptyCell.classList.add("calendar__day", "empty");
-      daysList.appendChild(emptyCell);
+      emptyCell.className = "calendar__day empty";
+      fragment.appendChild(emptyCell);
     }
 
     for (let day = 1; day <= daysInMonth; day++) {
       const dayCell = document.createElement("li");
-      dayCell.classList.add("calendar__day");
+      dayCell.className = "calendar__day";
       dayCell.textContent = day;
 
       if (
@@ -37,32 +36,25 @@
         month === today.getMonth() &&
         year === today.getFullYear()
       ) {
-        dayCell.classList.add("today");
+        dayCell.className = "calendar__day today";
       }
 
-      daysList.appendChild(dayCell);
+      fragment.appendChild(dayCell);
     }
+
+    daysList.innerHTML = "";
+    daysList.appendChild(fragment);
   };
 
   nextButton.addEventListener("click", () => {
-    currentMonth--;
-
-    if (currentMonth < 0) {
-      currentMonth = 11;
-      currentYear--;
-    }
-
+    currentMonth = (currentMonth + 1) % 12;
+    currentYear += currentMonth === 0 ? 1 : 0;
     renderCalendar(currentMonth, currentYear);
   });
 
   prevButton.addEventListener("click", () => {
-    currentMonth++;
-
-    if (currentMonth > 11) {
-      currentMonth = 0;
-      currentYear++;
-    }
-
+    currentMonth = (currentMonth - 1 + 12) % 12;
+    currentYear -= currentMonth === 11 ? 1 : 0;
     renderCalendar(currentMonth, currentYear);
   });
 
