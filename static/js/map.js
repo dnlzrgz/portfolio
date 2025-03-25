ef0383c585ad144971be75e38c8ac79ef3f29454
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2025-03-25 18:19:13 +0100

    refactor: remove dark mode and add flashcards

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,8 @@
-const map = L.map("map", {
-  attributionControl: false,
-  zoomControl: false,
-}).setView([46.51682, 6.62914], 12);
+{
+  const map = L.map("map", {
+    attributionControl: false,
+    zoomControl: false,
+  }).setView([46.51682, 6.62914], 12);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {}).addTo(map);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {}).addTo(map);
+}
