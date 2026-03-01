11e8ff2426b144aa07c15bca7a2f60c51517b4f2
daniel <contact@dnlzrgz.com>
2026-03-01 20:39:32 +0100

    feat: init commit

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+"""
+URL configuration for core project.
+"""
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("cms.urls")),
+]
